e76ff2e161d4358ae4e1b5810beeec0750c95cdd
Anthony Rabbito <hello@anthonyrabbito.com>
2022-01-11 08:40:20 -0500

    fix: remove archive mode
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- github-fetch.sh
+++ github-fetch.sh
@@ -11,5 +11,5 @@ echo "INFO: Downloading $ZIP ..."
 [ -e /tmp/repository.zip ] || $CURL -L -o /tmp/repository.zip "$ZIP"
 mkdir /tmp/repo
 unzip -p /tmp/repository.zip | tar -xzC /tmp/repo
-rsync --no-perms --no-group --no-owner -aP --delete /tmp/repo/ /data
+rsync --no-perms --no-group --no-owner -P --delete /tmp/repo/ /data
 rm -rf /tmp/**
